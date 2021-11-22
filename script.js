8e430d9fe02c678da8bc500f1419cacac63afeb8
edenpaulocruz <edenpaulo@gmail.com>
2021-11-22 03:44:14 -0300

    Definindo as variáveis do document

---
+++ script.js
@@ -0,0 +1,10 @@
+(function () {
+  'use strict'
+
+  const startButton = document.querySelector('[data-js="start-button"]')
+  const greenButton = document.querySelector('[data-js="green-button"]')
+  const redButton = document.querySelector('[data-js="red-button"]')
+  const blueButton = document.querySelector('[data-js="blue-button"]')
+  const yellowButton = document.querySelector('[data-js="yellow-button"]')
+  console.log(startButton, greenButton, redButton, blueButton, yellowButton)
+})();
