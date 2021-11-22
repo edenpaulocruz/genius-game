ef77bcd1372e369f06d7223a876005122c0c3976
edenpaulocruz <edenpaulo@gmail.com>
2021-11-22 04:28:11 -0300

    Redefinindo variáveis, adicionando função para acender cor ao clicar

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 (function () {
   'use strict'
 
-  const startButton = document.querySelector('[data-js="start-button"]')
-  const greenButton = document.querySelector('[data-js="green-button"]')
-  const redButton = document.querySelector('[data-js="red-button"]')
-  const blueButton = document.querySelector('[data-js="blue-button"]')
-  const yellowButton = document.querySelector('[data-js="yellow-button"]')
-  console.log(startButton, greenButton, redButton, blueButton, yellowButton)
+  // const startButton = document.querySelector('[data-js="start-button"]')
+  
+  const geniusButtons = [
+    document.querySelector('[data-js="green-button"]'),
+    document.querySelector('[data-js="red-button"]'),
+    document.querySelector('[data-js="blue-button"]'),
+    document.querySelector('[data-js="yellow-button"]')
+  ]
+  
+  const lightColor = element => {
+    element.classList.add('selected')
+
+    setTimeout(() => {
+      element.classList.remove('selected')
+    }, 350)
+  }
+
+  geniusButtons.forEach(button => button.onclick = () => lightColor(button))
 })();
