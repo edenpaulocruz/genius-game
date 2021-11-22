9f9ae8981cb095fe3f07cc3613eb9016a501424a
edenpaulocruz <edenpaulo@gmail.com>
2021-11-22 09:13:04 -0300

    Iniciando o jogo

--- script.js
+++ script.js
@@ -30,21 +30,51 @@
   async function shuffleOrder() {
     let colorOrder = Math.floor(Math.random() * 4)
     order.push(colorOrder)
-    console.log(order)
+    clickedOrder = []
     
     for (let count in order) {
       await lightColor(geniusButtons[order[count]])
     }
   }
 
-  const playGame = () => {
+  const resetGame = () => {
     score = 0
+    order = []
+  }
+
+  const playGame = () => {
+    resetGame()
     shuffleOrder()
+  }
+
+  const checkOrder = () => {
+    for (let j in clickedOrder) {
+      if (clickedOrder[j] != order[j]) {
+        return gameOver()
+      } 
+    }
+
+    if (clickedOrder.length === order.length) {
+      return nextLevel()
+    }
+  }
+
+  const nextLevel = () => {
+    score++
+    console.log(`Você acertou! Sua pontuação é ${score}!`)
+  }
+
+  const gameOver = () => {
+    resetGame()
+    console.log('Você errou!')
   }
   
   geniusButtons.forEach((button, index) => button.onclick = () => {
-    lightColor(button)
     clickedOrder.push(index)
+    lightColor(button)
+    setTimeout(() => {
+      checkOrder()
+    }, 500)
     console.log(clickedOrder)
   })
 
